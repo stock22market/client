39c739c23aa011d071b9166583135c7ab02692c6
Paghdal <619238@cognizant.com>
2020-07-07 08:56:36 +0530

    build

---
+++ build/precache-manifest.b6fbdbb11a753d9feea284e420491512.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c81ec0c1ab22f7fac460d562f67d2e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93f5d7c3866101eafd85",
+    "url": "/static/css/main.8cd522d4.chunk.css"
+  },
+  {
+    "revision": "1d8ce4a06b9ddd34dd85",
+    "url": "/static/js/0.ba23ea61.chunk.js"
+  },
+  {
+    "revision": "98d0f9f4ada1dceee38d",
+    "url": "/static/js/3.64b37885.chunk.js"
+  },
+  {
+    "revision": "bb9330914f22cc44d125",
+    "url": "/static/js/4.4636b928.chunk.js"
+  },
+  {
+    "revision": "0c348228bb0e1d48cba4",
+    "url": "/static/js/5.17a2ee35.chunk.js"
+  },
+  {
+    "revision": "52aa2885972f2c2bf8e7",
+    "url": "/static/js/6.283b7658.chunk.js"
+  },
+  {
+    "revision": "93f5d7c3866101eafd85",
+    "url": "/static/js/main.799f73a6.chunk.js"
+  },
+  {
+    "revision": "f5f97a5b240d00371692",
+    "url": "/static/js/runtime~main.1b20e495.js"
+  }
+]);
